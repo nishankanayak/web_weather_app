066d013c268e2edc240b9545b8b4e0fe9c0a3d8b
pythongit <nayak4nayak4@gmail.com>
2023-06-13 19:49:12 +0530

    get the values of temperature and sky condition from the function

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import streamlit as st
 import plotly.express as px
+from b_end import get_data
 
 st.title("Weather Forecast for the Next Days")
-place=st.text_input("Place: ")
+place=st.text_input("City: ")
 days=st.slider("Forecast Days",min_value=1,max_value=5,help="Select the number of forcasted days")
-option=st.selectbox("Select data to viwe",("Temperature","Sky"))
+option=st.selectbox("Select data to view",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    date=["2022-25-10","2022-26-10","2022-27-10"]
-    temperature=["20","25","39"]
-    temperature=[days*i for i in temperature]
-    return date,temperature
-
+data=get_data(place,days,option)
 d,t=get_data(days)
 
 figure=px.line(x=d,y=t,labels={"x":"Date","y":"Temperature (C)"})
