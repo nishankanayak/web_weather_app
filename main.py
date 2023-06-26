35ad9ac8d29095b42dd20e682b3b93efa5db55a2
pythongit <nayak4nayak4@gmail.com>
2023-06-26 16:25:28 +0530

    final

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import plotly.express as px
 from b_end import get_data
 
 st.title("Weather Forecast for the Next Days")
-place=st.text_input("City: ")
+place=st.text_input("City:")
 days=st.slider("Forecast Days",min_value=1,max_value=5,help="Select the number of forcasted days")
 option=st.selectbox("Select data to view",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
